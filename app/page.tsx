7b910c6be58115f7f114532a57a9a3a85ba3c584
matheusgleite <matheusps4efootebol@gmail.com>
2025-07-31 15:57:41 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold text-white">FutVirtualScanner</h1>
+      <p className="text-gray-400">Monitoramento avançado de ligas virtuais Bet365</p>
+      {/* Você pode adicionar aqui os gráficos e componentes */}
+    </main>
+  );
+}
